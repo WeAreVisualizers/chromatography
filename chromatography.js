b4e1552f1b73b54f311e06fa2b066b4daf747e6a
rich-iannone <riannone@me.com>
2015-02-25 17:04:04 -0800

    Add several new 'chromato.*' formatting functions

--- chromatography.js
+++ chromatography.js
@@ -10,8 +10,17 @@ chromato.color = function(x, y, z, m) {
   return new Color(x, y, z, m);
 };
 
-chromato.hex = function(x) {
-  return new Color(x);
-};
+  chromato.color = function(x, y, z, m) {
+    return new Color(x, y, z, m);
+  };
+  
+  chromato.rgb = function(r, g, b, a) {
+    return new Color(r, g, b, a, 'rgb');
+  };
+
+  chromato.hex = function(x) {
+    return new Color(x);
+  };
+  
 
 }).call(this);
