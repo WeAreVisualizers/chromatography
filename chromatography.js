47c472a55c53867bac622aeefcad873f797af5ae
rich-iannone <riannone@me.com>
2015-02-28 15:44:01 -0800

    Add 'Color.hsl2rgb' function

--- chromatography.js
+++ chromatography.js
@@ -220,6 +220,41 @@
     }
     return [h, s, v];
   };
+
+  Color.hsl2rgb = function(h, s, l) {
+    var b, c, g, i, r, t1, t2, t3, _ref2, _ref3;
+    if (h !== void 0 && h.length === 3) {
+      _ref2 = h, h = _ref2[0], s = _ref2[1], l = _ref2[2];
+    }
+    if (s === 0) {
+      r = g = b = l * 255;
+    } else {
+      t3 = [0, 0, 0];
+      c = [0, 0, 0];
+      t2 = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      t1 = 2 * l - t2;
+      h /= 360;
+      t3[0] = h + 1 / 3;
+      t3[1] = h;
+      t3[2] = h - 1 / 3;
+      for (i = 0; i <= 2; i++) {
+        if (t3[i] < 0) t3[i] += 1;
+        if (t3[i] > 1) t3[i] -= 1;
+        if (6 * t3[i] < 1) {
+          c[i] = t1 + (t2 - t1) * 6 * t3[i];
+        } else if (2 * t3[i] < 1) {
+          c[i] = t2;
+        } else if (3 * t3[i] < 2) {
+          c[i] = t1 + (t2 - t1) * ((2 / 3) - t3[i]) * 6;
+        } else {
+          c[i] = t1;
+        }
+      }
+      _ref3 = [Math.round(c[0] * 255), Math.round(c[1] * 255), Math.round(c[2] * 255)], r = _ref3[0], g = _ref3[1], b = _ref3[2];
+    }
+    return [r, g, b];
+  };
+
   };
 
   };
