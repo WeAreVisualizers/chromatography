14b45a4c566320a2205da67b7941f0665bd1560b
rich-iannone <riannone@me.com>
2015-02-28 15:39:47 -0800

    Remove 'chromato.*' functions

--- chromatography.js
+++ chromatography.js
@@ -124,32 +124,16 @@
 
   })();
   };
-  
-  chromato.rgb = function(r, g, b, a) {
-    return new Color(r, g, b, a, 'rgb');
   };
 
-  chromato.hex = function(x) {
-    return new Color(x);
   };
-  
-  chromato.hsl = function(h, s, l, a) {
-    return new Color(h, s, l, a, 'hsl');
   };
 
-  chromato.hsv = function(h, s, v, a) {
-    return new Color(h, s, v, a, 'hsv');
   };
 
-  chromato.lab = function(l, a, b) {
-    return new Color(l, a, b, 'lab');
   };
 
-  chromato.lch = function(l, c, h) {
-    return new Color(l, c, h, 'lch');
   };
-  
-  chromato.css = function(x) {
     return new Color(x);
   };
 
@@ -157,13 +141,8 @@
     return new Color(h, s, i, 'hsi');
   };
 
-  chromato.gl = function(r, g, b, a) {
-    return new Color(r * 255, g * 255, b * 255, a, 'gl');
   };
 
-  chromato.interpolate = function(a, b, f, m) {
-    if ((a == null) || (b == null)) {
-      return '#000';
     }
     if (type(a) === 'string') {
       a = new Color(a);
@@ -174,10 +153,7 @@
     return a.interpolate(f, b, m);
   };
 
-  chromato.mix = chromato.interpolate;
 
-  chromato.contrast = function(a, b) {
-    var l1, l2;
 
     if (type(a) === 'string') {
       a = new Color(a);
@@ -194,8 +170,6 @@
     }
   };
 
-  chromato.luminance = function(color) {
-    return chroma(color).luminance();
   };
 
 }).call(this);
