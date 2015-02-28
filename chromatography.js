76217764ea92609a71e1f552a23413d63165a3e0
rich-iannone <riannone@me.com>
2015-02-28 09:29:21 -0800

    Add 'Color.prototype.interpolate' function

--- chromatography.js
+++ chromatography.js
@@ -72,6 +72,62 @@ if ((typeof module !== "undefined" && module !== null) && (module.exports != nul
     Color.prototype.hsi = function() {
       return Color.rgb2hsi(this.rgb);
     };
+
+    Color.prototype.interpolate = function(f, col, m) {
+
+      var dh, hue, hue0, hue1, lbv, lbv0, lbv1, me, sat, sat0, sat1, xyz0, xyz1;
+      me = this;
+      if (m == null) m = 'rgb';
+      if (type(col) === 'string') col = new Color(col);
+      if (m === 'hsl' || m === 'hsv' || m === 'hcl' || m === 'hsi') {
+        if (m === 'hsl') {
+          xyz0 = me.hsl();
+          xyz1 = col.hsl();
+        } else if (m === 'hsv') {
+          xyz0 = me.hsv();
+          xyz1 = col.hsv();
+        } else if (m === 'hcl') {
+          xyz0 = me.hcl();
+          xyz1 = col.hcl();
+        } else if (m === 'hsi') {
+          xyz0 = me.hsi();
+          xyz1 = col.hsi();
+        }
+        hue0 = xyz0[0], sat0 = xyz0[1], lbv0 = xyz0[2];
+        hue1 = xyz1[0], sat1 = xyz1[1], lbv1 = xyz1[2];
+        if (!isNaN(hue0) && !isNaN(hue1)) {
+          if (hue1 > hue0 && hue1 - hue0 > 180) {
+            dh = hue1 - (hue0 + 360);
+          } else if (hue1 < hue0 && hue0 - hue1 > 180) {
+            dh = hue1 + 360 - hue0;
+          } else {
+            dh = hue1 - hue0;
+          }
+          hue = hue0 + f * dh;
+        } else if (!isNaN(hue0)) {
+          hue = hue0;
+          if (lbv1 === 1 || lbv1 === 0) sat = sat0;
+        } else if (!isNaN(hue1)) {
+          hue = hue1;
+          if (lbv0 === 1 || lbv0 === 0) sat = sat1;
+        } else {
+          hue = void 0;
+        }
+        if (sat == null) sat = sat0 + f * (sat1 - sat0);
+        lbv = lbv0 + f * (lbv1 - lbv0);
+        return new Color(hue, sat, lbv, m);
+      } else if (m === 'rgb') {
+        xyz0 = me.rgb;
+        xyz1 = col.rgb;
+        return new Color(xyz0[0] + f * (xyz1[0] - xyz0[0]), xyz0[1] + f * (xyz1[1] - xyz0[1]), xyz0[2] + f * (xyz1[2] - xyz0[2]), m);
+      } else if (m === 'lab') {
+        xyz0 = me.lab();
+        xyz1 = col.lab();
+        return new Color(xyz0[0] + f * (xyz1[0] - xyz0[0]), xyz0[1] + f * (xyz1[1] - xyz0[1]), xyz0[2] + f * (xyz1[2] - xyz0[2]), m);
+      } else {
+        throw m + ' is not supported as a color mode';
+      }
+    };
   };
   
   chromato.rgb = function(r, g, b, a) {
