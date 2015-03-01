963206aa2f053ffcc62305f2f7274edeb6b6e298
rich-iannone <riannone@me.com>
2015-02-28 22:16:37 -0800

    Add 'Array.max' and 'Array.min' functions

--- chromatography.js
+++ chromatography.js
@@ -704,8 +704,12 @@
     return limits;
   };
     }
+  Array.max = function(array) {
+    return Math.max.apply(Math, array);
   };
 
+  Array.min = function(array) {
+    return Math.min.apply(Math, array);
   };
 
 }).call(this);
