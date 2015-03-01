0bd9ae7dd35d85ae8b9540be1204a5d2e4aff74b
rich-iannone <riannone@me.com>
2015-02-28 22:18:35 -0800

    Add several initializers to 'chromato.limits'

--- chromatography.js
+++ chromatography.js
@@ -677,6 +677,13 @@
 
   chromato.limits = function(data, mode, num, prop) {
     var assignments, best, centroids, cluster, clusterSizes, dist, i, j, k, kClusters, limits, max, min, mindist, n, nb_iters, newCentroids, p, pb, pr, repeat, row, sum, tmpKMeansBreaks, val, value, values, _i, _j, _k, _len, _len2, _len3, _ref10, _ref11, _ref12, _ref13, _ref14, _ref15, _ref16, _ref3, _ref4, _ref5, _ref6, _ref7, _ref8, _ref9;
+    if (mode == null) mode = 'equal';
+    if (num == null) num = 7;
+    if (prop == null) prop = null;
+    min = Number.MAX_VALUE;
+    max = Number.MAX_VALUE * -1;
+    sum = 0;
+    values = [];
     if (type(data) === 'array') {
       if (type(data[0]) !== 'object' && type(data[0]) !== 'array') {
         for (_i = 0, _len = data.length; _i < _len; _i++) {
