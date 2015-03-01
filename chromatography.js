add8f58b64f4f6a22e22197141a757a811b29fe5
rich-iannone <riannone@me.com>
2015-02-28 22:35:23 -0800

    Add var statement for prototype functions

--- chromatography.js
+++ chromatography.js
@@ -1,6 +1,7 @@
 (function () {
 
   var Color, ColorScale, chromato, CSSColors, Ramp, _ref, _ref2, _ref3;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
 
   Color = (function() {
